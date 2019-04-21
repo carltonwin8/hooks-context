89d8529772faef66625b5124354d1358a9db738a
Carlton Joseph <carlton.joseph@gmail.com>
2019-04-21 14:19:41 -0700

    cj initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useContext, useReducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const UserContext = React.createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const [user, setUser] = useState({
+    firstName: "Carlton",
+    lastName: "Joseph"
+  });
+
+  return (
+    <UserContext.Provider value={user}>
+      <Main />
+    </UserContext.Provider>
+  );
+};
+
+const Navbar = () => {
+  const { firstName, lastName } = useContext(UserContext);
+  return (
+    <nav>
+      <ol>
+        <li>Cool App</li>
+        <li>
+          Hello, {firstName} {lastName}
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>App Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum
+          quam ad voluptatem! Fuga maiores tenetur est quos, laboriosam soluta,
+          dolorem optio eaque maxime suscipit debitis commodi vel necessitatibus
+          explicabo?
+        </p>
+        <CountReducer />
+        <CountuseState />
+      </main>
+    </>
+  );
+};
+
+const CountReducer = () => {
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case "-":
+        return --state;
+      case "+":
+        return ++state;
+      default:
+        return state;
+    }
+  }, 0);
+  return (
+    <p>
+      <span onClick={() => dispatch("-")}>-</span> {count}{" "}
+      <span onClick={() => dispatch("+")}>+</span> useReducer
+    </p>
+  );
+};
+
+const CountuseState = () => {
+  const [count, setCount] = useState(0);
+  const inc = () => setCount(p => ++p);
+  const dec = () => setCount(p => --p);
+  return (
+    <p>
+      <span onClick={inc}>-</span> {count} <span onClick={dec}>+</span> useState
+    </p>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
